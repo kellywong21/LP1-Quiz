e15b76a0f2201bfee7e1676d1dfaa899b9dece1c
18004885 <18004885@myrp.edu.sg>
2020-05-08 22:31:18 +0800

    FINISH

--- app/src/main/java/com/myapplicationdev/android/lp1_quiz/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/lp1_quiz/MainActivity.java
@@ -3,10 +3,12 @@ package com.myapplicationdev.android.lp1_quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -17,12 +19,34 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lvMood;
+    EditText etMood;
+    Button btnSubmit;
+    ArrayAdapter aa;
+    ArrayList<Mood> moodList;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvMood = findViewById(R.id.lvMood);
+        etMood = findViewById(R.id.etMood);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        moodList = new ArrayList<Mood>();
+
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mood = etMood.getText().toString();
+                moodList.add(new Mood(mood,getDate()));
+                aa = new MoodAdapter(MainActivity.this,R.layout.row,moodList);
+                lvMood.setAdapter(aa);
+                aa.notifyDataSetChanged();
+
+            }
+        });
     }
 
     protected String getDate(){
@@ -32,5 +56,6 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Today's date : ", formattedDate);
         return formattedDate;
     }
+
 
 }
